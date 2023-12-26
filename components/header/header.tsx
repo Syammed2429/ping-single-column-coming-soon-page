46a2df97a42b1fa96fec277188c3b2eb8dd0ca2e
Dada Khalandar <kerry.blig12@gmail.com>
2023-12-26 17:08:00 +0800

    completed the header component

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
 export const Header = () => {
-  return <div>PING.</div>;
+  return (
+      <div className='text-center mt-14'>
+        <p className='text-[2rem] font-bold mb-10'>
+          PING<span className='text-[#4C7BF3]'>.</span>
+        </p>
+        <p className='text-[#969696] text-5xl font-light mb-5'>
+          We are launching{''}
+          <span className='text-[#15202A] font-bold'> soon!</span>
+        </p>
+        <p className='text-[#15202A] text-xl font-light'>
+          Subscribe and get notified
+        </p>
+      </div>
+  );
 };
