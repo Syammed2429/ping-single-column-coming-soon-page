46ef63b66d19c1843d856b2ad0cab3230bcaefe6
Dada Khalandar <kerry.blig12@gmail.com>
2023-12-26 18:26:32 +0800

    added the image

---
+++ components/image/image-component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import mainImage from '@/public/assets/images/mainImage.svg';
+import Image from 'next/image';
+
+const ImageComponent = () => {
+  return (
+    <div className='align-middle my-[5rem]'>
+      <Image src={mainImage} alt='mainImage' />
+    </div>
+  );
+};
+
+export default ImageComponent;
--- container/home/home-container.tsx
+++ container/home/home-container.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Header } from '@/components/header/header';
 import { NotifyMe } from '@/components/notify-me/notify-me';
+import ImageComponent from '@/components/image/image-component';
 
 export const HomeContainer = () => {
   return (
     <div className='flex justify-center items-center flex-col'>
       <Header />
       <NotifyMe />
-      
+      <ImageComponent />
     </div>
   );
 };
