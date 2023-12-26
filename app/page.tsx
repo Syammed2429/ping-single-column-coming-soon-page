4a6339260f80779d2e0581ccb222231b5cf1b1c4
Dada Khalandar <kerry.blig12@gmail.com>
2023-12-26 16:59:32 +0800

    creating the home container

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image';
+import { HomeContainer } from '@/container/home/home-container';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <HomeContainer />
+    </div>
+  );
 }
---
+++ container/home/home-container.tsx
@@ -0,0 +1,10 @@
+import { Header } from '@/components/header/header';
+import React from 'react';
+
+export const HomeContainer = () => {
+  return (
+    <div className='flex justify-center '>
+      <Header />
+    </div>
+  );
+};
